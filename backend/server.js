5d5dce78d6f3eb68d4d11c95902ab9c6e81c5a1d
abdullah-dot-programmer <a22795101@gmail.com>
2024-07-29 09:37:33 +0500

    Deploying on server procedure added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
+import path from "path"
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -12,8 +13,10 @@ import cors from "cors"
 import { app, server } from "./socket/socket.js"
 
 
+
 dotenv.config();
 
+const __dirname = path.resolve()
 const PORT = process.env.PORT;
 
 app.use(express.json()); //to parse the incoming data as json payload (from req.body)
@@ -26,6 +29,11 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 // app.get("/", (req, res) => {
 //     //root route
